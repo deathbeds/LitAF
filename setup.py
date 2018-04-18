da8b8fc8f13a6dda9f71cd396dd4fa07c7f94720
tonyfast <tony.fast@gmail.com>
2018-04-18 13:14:06 -0400

    Ah, push it  Ah, push it  Oooh, baby, baby

---
+++ setup.py
@@ -0,0 +1,11 @@
+__import__('setuptools').setup(
+    name="litaf",
+    version="0.0.1",
+    author="tonyfast", author_email="tony.fast@gmail.com",
+    description="Lit as fuck.", 
+    license="BSD-3-Clause",
+    install_requires=[],
+    include_package_data=True,
+    packages=['litaf'],
+    
+)
